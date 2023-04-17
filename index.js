d1333b0ac3c1741de0852a49d5e88906d8202721
ManojKumarNamadi61 <manojkumarnamadi14999@gmail.com>
2023-04-17 21:02:58 +0530

    added

---
+++ index.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 300,
+        modifier: 1,
+        slideShadows: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+
+function myFunction() {
+  alert("Your response is with us. Thank You!");
+}
